7d292325873a393446ce8171f63e4c6a1dc0fdba
jinghong1111 <49048844+jinghong1111@users.noreply.github.com>
2021-11-14 11:34:19 -0500

    Update data-visualization.py

--- data-visualization.py
+++ data-visualization.py
@@ -11,30 +11,30 @@ while (x!= 'n'):
 
 def user_graph():
     print("How would you like to view your result compare to the average people surveyed in U.S?\n ")
-    ask = input("a. Gender, b. Race, c. Age").lower()
-    while not( ask == 'a' or ask == 'b' or ask == 'c'):
-        ask = input("Please enter valid response: (a/b/c)\n")
-    if ask == 'a':
+    q = input("a. Gender, b. Race, c. Age\n").lower()
+    while not( q == 'a' or q == 'b' or q == 'c'):
+        q = input("Please enter valid response: (a/b/c)\n")
+    if q == 'a':
         gender()
-    elif ask == 'b':
+    elif q == 'b':
         race()
-    else:
+    elif q == 'c':
         age()
     
 def depression_stats(): # base line stats that we will compare the average sample population with 
     all_x = ["male", "female", "other",
         "Natives", "Black", "Hispanic", "Pacific Islander", "White", "Asian",
         "10-17", "18-25", "26-49", "50+",
-        "Diagnosed by doc",
+        "Diagnosed by dr",
         "treated"]
     all_stats = [ 6.2, 10.5, 22.1, 18.7, 17.3, 18.0, 16.6, 22.2, 14.4, 17.2, 20.3, 9.1, 5.4, 7.8, 32.2] 
     x_pos_all = [i for i, _ in enumerate(all_x)]
     fig, axes = plt.subplots(1, 1, figsize=(20, 11))
     plt.bar(x_pos_all[0:3], all_stats[0:3], color='green')
     plt.bar(x_pos_all[3:9], all_stats[3:9], color='cyan')
-    plt.bar(x_pos_all[9:12], all_stats[9:12], color='pink')
-    plt.bar(x_pos_all[12:13], all_stats[12:13], color='blue')
-    plt.bar(x_pos_all[13:], all_stats[13:], color='purple')
+    plt.bar(x_pos_all[9:13], all_stats[9:13], color='pink')
+    plt.bar(x_pos_all[13:14], all_stats[13:14], color='blue')
+    plt.bar(x_pos_all[14:], all_stats[14:], color='purple')
 
     plt.xlabel("Categories", fontweight = "bold")
     plt.ylabel("Percentage diagnosed with depression", fontweight = "bold")
@@ -60,7 +60,7 @@ def gender():
     # Bar Graph By Gender 
     gender = ["male", "female", "other"]
     y_gender = [6.2, 10.5, 22.1 ]   # in the order of male, female, other. Source: https://www.nami.org/mhstats
-    ig, axes = plt.subplots(1, 1, figsize=(13, 10))
+    fig, axes = plt.subplots(1, 1, figsize=(13, 10))
     plt.bar(gender , y_gender, width=0.2, color='b', align='center')
     plt.xlabel("Gender")
     plt.ylabel("Percentage diagnosed with depression")
@@ -69,7 +69,7 @@ def gender():
     return gender, y_gender
 def age():
     # Bar Graph by Age Group: 
-    age = ["10-17, 18-25", "26-49", "50+"] # age group survyed 
+    age = ["10-17", "18-25", "26-49", "50"] # age group survyed 
     y_age = [17.2, 20.3, 9.1, 5.4 ]       # in the order of the age group above. source: https://www.nami.org/mhstats
     fig, axes = plt.subplots(1, 1, figsize=(13, 10))
     plt.bar(age , y_age, width=0.2, color='b', align='center')
@@ -77,7 +77,6 @@ def age():
     plt.ylabel("Percentage diagnosed with depression")
     plt.title("Depression: Age", color = 'red', fontweight = "bold")
     plt.show()
-    return age, y_age
 
 def cumulation(user_list):
     dep_score = 0
@@ -94,6 +93,4 @@ def cumulation(user_list):
 
 user_graph()
 depression_stats()
-#cumulation(user_list)
-
-#def comparison(survey, average):
+cumulation(user_list)
